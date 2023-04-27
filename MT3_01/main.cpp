a22d5c43cf0183cf4d5ed92c734c8cddfabc6710
俊仁 小林 <takigaya0210@gmail.com>
2023-04-27 18:25:43 +0900

    kara

--- MT3_01/main.cpp
+++ MT3_01/main.cpp
@@ -25,6 +25,12 @@ void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix) {
 		}
 	}
 }
+void vectorScreenprintf(int x, int y, const Vector3& vector, const char* label) {
+	Novice::ScreenPrintf(x, y, "%.02f", vector.x);
+	Novice::ScreenPrintf(x + kColumnWidth, y, "%.02f", vector.y);
+	Novice::ScreenPrintf(x + kColumnWidth * 2, y, "%.02f", vector.z);
+	Novice::ScreenPrintf(x + kColumnWidth * 3, y, "%s", label);
+}
 
 
 // Windowsアプリでのエントリーポイント(main関数)
